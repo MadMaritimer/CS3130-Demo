ef2fcebddecc17349a0915d8b6c0ccc6bca6cc7b
Ben <parker.benjaminr@gmail.com>
2017-05-11 07:26:30 -0300

    Added Button and input field. When button is pressed, text from new field copied into existing field

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnPress(View view) {
+        TextView hello = (TextView) findViewById(R.id.helloText);
+        EditText input  = (EditText) findViewById(R.id.userText);
+        hello.setText(input.getText());
+        input.setText("");
     }
 }
